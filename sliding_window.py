fa965e621b981f3ba1726167080e35f234be0369
xelathan <alex.t.tran@gmail.com>
2023-11-29 11:05:39 -0800

    finished stop/wait and sliding window

---
+++ sliding_window.py
@@ -0,0 +1,59 @@
+import socket
+import time
+
+PACKET_SIZE = 1024
+SEQ_ID_SIZE = 4
+MESSAGE_SIZE = PACKET_SIZE - SEQ_ID_SIZE
+SLIDING_WINDOW_SIZE = 100
+
+with open('send.txt', 'rb') as f:
+    data = f.read()
+
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp_socket:
+    start_time_tp = time.time()
+    udp_socket.bind(("0.0.0.0", 5000))
+    udp_socket.settimeout(1)
+
+    window_pointer_id = 0
+    packet_delays = []
+    while window_pointer_id < len(data):
+        messages = []
+        acks = {}
+        current_pointer_id = window_pointer_id
+
+        for i in range(SLIDING_WINDOW_SIZE):
+            message = int.to_bytes(current_pointer_id, SEQ_ID_SIZE, byteorder='big', signed=True) + \
+                      data[current_pointer_id:current_pointer_id + MESSAGE_SIZE]
+            messages.append((current_pointer_id, message))
+            acks[current_pointer_id] = False
+            current_pointer_id += MESSAGE_SIZE
+
+        for _, message in messages:
+            start_delay = time.time()
+            udp_socket.sendto(message, ('localhost', 5001))
+
+        while True:
+            try:
+                ack, _ = udp_socket.recvfrom(PACKET_SIZE)
+                end_delay = time.time()
+                packet_delays.append(end_delay - start_delay)
+                ack_id = int.from_bytes(ack[:SEQ_ID_SIZE], byteorder='big')
+                print(ack_id, ack[SEQ_ID_SIZE:])
+                acks[ack_id] = True
+
+                # all acks received, move on
+                if all(acks.values()):
+                    break
+            except socket.timeout:
+                # no ack received, resend unacked messages
+                for sid, message in messages:
+                    if not acks[sid]:
+                        udp_socket.sendto(message, ('localhost', 5001))
+
+        window_pointer_id += MESSAGE_SIZE * SLIDING_WINDOW_SIZE
+
+    end_time_tp = time.time()
+    udp_socket.sendto(int.to_bytes(-1, 4, signed=True, byteorder='big'), ('localhost', 5001))
+
+print(f"throughput: {len(data) / (end_time_tp - start_time_tp)} bytes per second")
+print(f"average delay per packet: {sum(packet_delays) / len(packet_delays)} seconds")
--- stop_and_wait.py
+++ stop_and_wait.py
@@ -4,7 +4,39 @@ import time
 PACKET_SIZE = 1024
 SEQ_ID_SIZE = 4
 MESSAGE_SIZE = PACKET_SIZE - SEQ_ID_SIZE
-WINDOW_SIZE = 20
 
 with open('send.txt', 'rb') as f:
     data = f.read()
+
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp_socket:
+    start_time_tp = time.time()
+    udp_socket.bind(("0.0.0.0", 5000))
+    udp_socket.settimeout(1)
+
+    pointer_id = 0
+    packet_delays = []
+    while pointer_id < len(data):
+        message = int.to_bytes(pointer_id, SEQ_ID_SIZE, byteorder='big', signed=True) +\
+            data[pointer_id:pointer_id + MESSAGE_SIZE]
+
+        start_delay = time.time()
+        udp_socket.sendto(message, ('localhost', 5001))
+        while True:
+            try:
+                ack, _ = udp_socket.recvfrom(PACKET_SIZE)
+                end_delay = time.time()
+                packet_delays.append(end_delay - start_delay)
+                ack_id = int.from_bytes(ack[:SEQ_ID_SIZE], byteorder='big')
+                print(ack_id, ack[SEQ_ID_SIZE:])
+                break
+            except socket.timeout:
+                print("timeout")
+                udp_socket.sendto(message, ('localhost', 5001))
+
+        pointer_id += MESSAGE_SIZE
+
+    end_time_tp = time.time()
+    udp_socket.sendto(int.to_bytes(-1, 4, signed=True, byteorder='big'), ('localhost', 5001))
+
+print(f"throughput: {len(data) / (end_time_tp - start_time_tp)} bytes per second")
+print(f"average delay per packet: {sum(packet_delays) / len(packet_delays)} seconds")
