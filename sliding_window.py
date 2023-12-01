c557b15cfad5369befbc9e5c59ee14041593d5e7
xelathan <alex.t.tran@gmail.com>
2023-11-30 17:37:47 -0800

    fixed sliding_window

--- sliding_window.py
+++ sliding_window.py
@@ -6,8 +6,10 @@ SEQ_ID_SIZE = 4
 MESSAGE_SIZE = PACKET_SIZE - SEQ_ID_SIZE
 SLIDING_WINDOW_SIZE = 100
 
-with open('send.txt', 'rb') as f:
+with open('congestion_control_ecs152a/docker/file.mp3', 'rb') as f:
     data = f.read()
+
+print(len(data))
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp_socket:
     start_time_tp = time.time()
@@ -37,15 +39,14 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp_socket:
                 ack, _ = udp_socket.recvfrom(PACKET_SIZE)
                 end_delay = time.time()
                 packet_delays.append(end_delay - start_delay)
-                ack_id = int.from_bytes(ack[:SEQ_ID_SIZE], byteorder='big')
+                ack_id = int.from_bytes(ack[:SEQ_ID_SIZE], byteorder='big') - MESSAGE_SIZE
                 print(ack_id, ack[SEQ_ID_SIZE:])
                 acks[ack_id] = True
 
-                # all acks received, move on
-                if all(acks.values()):
+                if all(acks.values()) or len(data) == ack_id + MESSAGE_SIZE:
                     break
             except socket.timeout:
-                # no ack received, resend unacked messages
+                print("timeout")
                 for sid, message in messages:
                     if not acks[sid]:
                         udp_socket.sendto(message, ('localhost', 5001))
@@ -53,7 +54,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp_socket:
         window_pointer_id += MESSAGE_SIZE * SLIDING_WINDOW_SIZE
 
     end_time_tp = time.time()
-    udp_socket.sendto(int.to_bytes(-1, 4, signed=True, byteorder='big'), ('localhost', 5001))
+    udp_socket.sendto(int.to_bytes(window_pointer_id, SEQ_ID_SIZE, byteorder='big', signed=True) + b'', ('localhost', 5001))
 
 print(f"throughput: {len(data) / (end_time_tp - start_time_tp)} bytes per second")
 print(f"average delay per packet: {sum(packet_delays) / len(packet_delays)} seconds")
